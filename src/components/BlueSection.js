0659ba6284da1b964c0f9e7868fd6c75de373224
John Michael De Guzman <jmxdg1029@gmail.com>
2021-03-14 20:16:42 -0400

    a

--- src/components/BlueSection.js
+++ src/components/BlueSection.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import blue_speaker from '../components/images/SmallSpeaker1-03.png';
+import blue_speaker2 from '../components/images/SmallSpeaker2-03.png';
+import '../components/BlueSection.css';
 
 class BlueSection extends Component {
     render() { 
         return (
         <div className="blue-section">
-
+            <a href="#" className="try_it_now float-right" ><span>TRY IT NOW</span></a>
+            <div className="row display-blue">
+                <div className="col">
+                    <img src={blue_speaker} className="blue-speaker"/>
+                    <img src={blue_speaker2} className="blue-speaker2"/>
+                </div>
+                <div className="col artist-display">
+                    <h1 className="text-white">REVIEWS</h1>
+                    <h2 className="text-white">ARTIST</h2>
+                    <p className="desc">"Love it, it's the Best.I can't live without it!"</p>
+                </div>
+                <div className="col produce-display">
+                    <h2 className="text-white">PRODUCER</h2>
+                    <p className="desc">"Love it, it's the Best.I can't live without it!"</p>
+                </div>
+                <div className="col music-display">
+                    <h2 className="text-white">MUSIC FAN</h2>
+                    <p className="desc">"Love it, it's the Best.I can't live without it!"</p>
+                </div>
+            </div>
         </div>);
     }
 }
